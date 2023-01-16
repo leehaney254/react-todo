7ce74ff88def7bbfbf578715fd631c63a76c25d9
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-16 18:51:18 +0300

    Feat: Complete app in classes

---
+++ src/components/InputTodo.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react"
+
+class InputTodo extends Component {
+  state = {
+    title: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      title: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title)
+      this.setState({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          onChange={this.onChange}
+          value={this.state.title} />
+        <button className="input-submit">Submit</button>
+      </form>
+    )
+  }
+}
+export default InputTodo;
