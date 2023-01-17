b24f6b238a1db658d09b8ce3a3ceb53e825cd9e7
Leehaney gee <georgeleehaney20@gmail.com>
2023-01-17 09:52:51 +0300

    Add navbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import "./App.css";
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TodoContainer from './functionalBased/components/TodoContainer';
+import About from './functionalBased/pages/About';
+import NotMatch from './functionalBased/pages/NotMatch';
+import Navbar from './functionalBased/components/Navbar';
 
-import TodoContainer from "./components/TodoContainer";
+import "././functionalBased/App.css";
 
-ReactDom.render(
+ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>, document.getElementById('root'));
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
